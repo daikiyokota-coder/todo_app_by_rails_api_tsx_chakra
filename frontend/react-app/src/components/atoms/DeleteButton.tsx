a924f633a06a89a57566741339fe3c36e59c8db5
yokotadaiki <yd43star@gmail.com>
2021-07-25 17:04:04 +0900

    削除ボタンコンポーネントを修正

--- frontend/react-app/src/components/atoms/DeleteButton.tsx
+++ frontend/react-app/src/components/atoms/DeleteButton.tsx
@@ -1,9 +1,8 @@
 import { Button } from "@chakra-ui/react";
 import { memo, ReactNode, VFC } from "react";
-import { Todo } from "../../interfaces/index"
 type Props = {
   children: ReactNode;
-  onClick: (todo: number) => void;
+  onClick: () => void;
 };
 
 export const DeleteButton: VFC<Props> = memo((props) => {
