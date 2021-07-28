8368b7cb255f46f0d6d6593314663a47cecea558
yokotadaiki <yd43star@gmail.com>
2021-07-28 17:50:55 +0900

    ヘッダー作成

--- frontend/react-app/src/App.tsx
+++ frontend/react-app/src/App.tsx
@@ -8,7 +8,7 @@ import { VFC } from "react"
 
 //chakra全体用！！！
 import { ChakraProvider } from "@chakra-ui/react"
-import { Container } from "@chakra-ui/react"
+import { Container, Heading, Flex } from "@chakra-ui/react"
 //chakra全体用！！！
 
 
@@ -34,8 +34,22 @@ const App: VFC = () => {
 
   return (
     <ChakraProvider>
+      <Flex
+        as="nav"
+        bg="teal.400"
+        color="gray.50"
+        padding={{ base: 3, md: 4 }}
+        mb="5"
+      >
+        <Heading
+          as="h1"
+          fontSize={{ base: "md", md: "lg" }}
+          ml="5"
+        >
+          Todo App
+        </Heading>
+      </Flex>
       <Container maxW="container.md">
-        <h1>Todo App</h1>
         <TodoForm todos={todos} setTodos={setTodos} />
         <TodoList todos={todos} setTodos={setTodos} />
       </Container>
